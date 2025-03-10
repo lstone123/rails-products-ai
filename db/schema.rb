adf159e653ace44246605c395b8f8e342ad1c09e
Louise Stone <louisemstone@hotmail.com>
2025-03-10 13:41:36 +0000

    created product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_10_133600) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_10_133853) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Product.destroy_all
+
+Product.create!(name: "Trousers", description: "A pair of bluejeans best suited for tall people.", price: 50)
+Product.create!(name: "Shirt", description: "A white shirt with a pocket.", price: 20)
+Product.create!(name: "Shoes", description: "A pair of black shoes.", price: 30)
+Product.create!(name: "Hat", description: "A baseball cap.", price: 10)
+Product.create!(name: "Chair", description: "A wooden chair.", price: 40)
+Product.create!(name: "Table", description: "A metal table.", price: 60)
+Product.create!(name: "Sofa", description: "A leather sofa.", price: 100)
+Product.create!(name: "Bed", description: "A queen-sized bed.", price: 200)
